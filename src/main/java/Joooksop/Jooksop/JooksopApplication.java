8e80da048f0dba9dac8781351bbcfe22128edf76
hewnyo <ggongju302@naver.com>
2025-04-08 17:12:51 +0900

    초기 프로젝트 설정

---
+++ src/main/java/Joooksop/Jooksop/JooksopApplication.java
@@ -0,0 +1,13 @@
+package Joooksop.Jooksop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooksopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooksopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Joooksop/Jooksop/JooksopApplicationTests.java
@@ -0,0 +1,13 @@
+package Joooksop.Jooksop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooksopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
