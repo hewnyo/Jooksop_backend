31dae98de21c07b82d9e3721b28ee83b4adf2f07
hewnyo <ggongju302@naver.com>
2025-06-10 13:38:27 +0900

    0610

--- src/main/java/com/sharediary/friend/domain/Friend.java
+++ src/main/java/com/sharediary/friend/domain/Friend.java
@@ -1,12 +1,8 @@
 package com.sharediary.friend.domain;
 
-<<<<<<< HEAD
-import jakarta.persistence.Id;
-import lombok.*;
-=======
+
 import lombok.*;
 import org.springframework.data.annotation.Id;
->>>>>>> hyewon
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "friends")
